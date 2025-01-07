643b37be93bd8e9ecff89b7ed5750b9ad57a0bff
XosuperiorXo <157197548+MAD-MAX77@users.noreply.github.com>
2025-01-07 10:13:48 +0530

    Update mentione.js

--- plugins/mentione.js
+++ plugins/mentione.js
@@ -13,11 +13,14 @@ cmd({
         m, client, args
     })=> {
       //  if ( MENTIONE === "true" ? "false" : true && false ) {
-        let img = "https://cdn.ironman.my.id/q/vBXgS.mp4"
-        let SUDO = "919497705819,919961857267";
+        let song = [ "https://cdn.ironman.my.id/q/yjryp.mp4","https://cdn.ironman.my.id/q/ywecS.mp4","https://cdn.ironman.my.id/q/zRSwS.mp4","https://cdn.ironman.my.id/q/FpJYh.mp4","https://cdn.ironman.my.id/q/wVHal.mp4",];
+        const songMsg = song[Math.floor(Math.random() * song.length)];
+        let img = ["https://cdn.ironman.my.id/q/zRfLV.jpg","https://cdn.ironman.my.id/q/xxBax.jpg","https://cdn.ironman.my.id/q/DhBxA.jpg","https://cdn.ironman.my.id/q/HElrw.jpg","https://cdn.ironman.my.id/q/etLJh.jpg",];
+        const imgMsg = img[Math.floor(Math.random() * img.length)];
+        let SUDO = "919497705819";
 if(SUDO.split(",").some((number) => args.includes(number))) {
-              const Audios = await (await fetch(img)).buffer()
-await client.sendMessage(m.jid,{audio: Audios,mimetype: 'audio/mp4', ptt: true,
+              const Audios = await (await fetch(songMsg)).buffer()
+await client.sendMessage(m.jid,{audio: Audios,mimetype: 'audio/mp4', ptt: false,
 contextInfo: 
 { externalAdReply: {
 title: "HY SUPERIOR IS HERE",
@@ -27,6 +30,6 @@ mediaUrl: "https://www.instagram.com/4.4.4.4.4.4.4444",
 mediaType: 1,
 showAdAttribution: true,
 renderLargerThumbnail: false,
-thumbnailUrl: " https://cdn.ironman.my.id/q/lYXtA.jpg"}} },{quoted: m })
+thumbnailUrl: imgMsg }} },{quoted: m })
     
 } });
