ca8e2846154d12d29d5175c0f1c86eb95bc69f89
ambaan2255 <newspotify4444@gmail.com>
2025-08-09 14:06:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,34 +1,30 @@
-const axios = require('axios');
-const express = require('express');
-const http = require('http');
-
+const axios = require("axios");
+const express = require("express");
+const http = require("http");
+//console.log("sᴇʀᴠᴇʀ sᴛᴀʀᴛᴇᴅ ✅")
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 6838;
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 let i = 0;
 
-async function web() {
-	console.log("web Starting...");
-	app.get('/', function(req, res) {
-		res.send({
-			status: "Active"
-		});
-	});
-	const server = http.createServer(app);
-	server.listen(PORT, async() => {
-		console.log('Connected to Server -- ', PORT);
-		while (true) {
-			i++;
-			try {
-				let response = await axios("https://newa1.onrender.com")
-				console.log("ᴄ ᴏ ɴ ɴ ᴇ ᴄ ᴛ ᴇ ᴅ  ᴛ ᴏ  s ᴇ ʀ ᴠ ᴇ ʀ  ✓ : ", response.status)
-				await sleep(40000)
-			} catch {
-				console.log("Retrying...")
-			}
-		}
-	});
-}
+async function web(connect) {
+    app.get('/', function (req, res) {
+        res.send(`${connect}`);
+    });
+  app.listen(PORT, () => console.log("sᴇʀᴠᴇʀ sᴛᴀʀᴛᴇᴅ ᴏɴ ᴘᴏʀᴛ : ", PORT));
+  while (true) {
+      i++;
+      try {
+       let response = await axios("http://web.v1p3r-x.repl.co")
+          //console.log('Connected to web -- ', response.status);
+          await sleep(40000)
+      } catch {
+        await sleep(10000)
+         //console.log("Retrying...");
+        
+      }
+  }
 
-module.exports = web
+    }
+ module.exports = web
